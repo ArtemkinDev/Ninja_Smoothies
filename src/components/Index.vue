96cbc159dc0a8f1d4f94217b6bc3deec1ac6b22f
Alex Artemkin <alehandro0289@gmail.com>
2019-09-22 23:20:40 +0300

    Nav

---
+++ src/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Index</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index'
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+        <div class="nav-content">
+            <router-link to="">
+                <span class="nav-title">Ninja Smoothies</span>
+            </router-link>
+            <router-link to="" class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+                <i class="material-icons">add</i>
+            </router-link>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+    .navbar .nav-extended {
+        margin-bottom: 50px;
+    }
+</style>
